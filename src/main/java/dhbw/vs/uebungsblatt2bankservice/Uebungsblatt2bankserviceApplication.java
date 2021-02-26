daddebc6a6f1343f4d48dedcd166cffb2aac0970
Elias <e.mueller24@gmx.de>
2021-02-26 12:51:52 +0100

    Initial commit

---
+++ src/main/java/dhbw/vs/uebungsblatt2bankservice/Uebungsblatt2bankserviceApplication.java
@@ -0,0 +1,13 @@
+package dhbw.vs.uebungsblatt2bankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uebungsblatt2bankserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Uebungsblatt2bankserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dhbw/vs/uebungsblatt2bankservice/Uebungsblatt2bankserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dhbw.vs.uebungsblatt2bankservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uebungsblatt2bankserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
